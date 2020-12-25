ccc8f94edb82e9f8860298fb90f087bcab5ab97b
AsmaKermiche <59176184+AsmaKermiche@users.noreply.github.com>
2020-12-25 09:39:19 +0100

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,331 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  double zoomVal=5.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.alarm),
+            onPressed: () {
+              //
+            }),
+        title: Text("New York"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                //
+              }),
+        ],
+      ),*/
+      body: Stack(
+        children: <Widget>[
+          _buildGoogleMap(context),
+          /*_zoomminusfunction(),
+          _zoomplusfunction(),*/
+          _buildContainer(),
+        ],
+      ),
+    );
+  }
+
+  Widget _zoomminusfunction() {
+
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+          icon: Icon(Icons.zoom_out,color:Colors.red),
+          onPressed: () {
+            zoomVal--;
+            _minus( zoomVal);
+          }),
+    );
+  }
+  Widget _zoomplusfunction() {
+
+    return Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+          icon: Icon(Icons.zoom_in,color:Colors.red),
+          onPressed: () {
+            zoomVal++;
+            _plus(zoomVal);
+          }),
+    );
+  }
+
+  Future<void> _minus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  }
+  Future<void> _plus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  }
+
+
+  Widget _buildContainer() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://batis.dz/images/94.jpg",
+                  36.73225, 3.08746,"Algiers","Bahi Manel","0526632114"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://www.pagesjaunes-dz.com/pub/pageweb/219447/219447-1.jpg",
+                  31.9500, 5.3167,"Ouargla","Benhamadi Yasmine","0785596223"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh3.googleusercontent.com/proxy/hv-isUvY6c0B3_o4yuQ79JZHfxZEudeO8aoZfZQaeD6oApNHIAASiFVLutPw5-YmvL0jw44Jj5RNh_JnGpQ_OUm6Vg8hLrFMVDT5Nj4VcyIrKEoEaQU2EeXKRPDfDg",
+                  36.9, 7.76667,"Annaba","Kermiche Asma", "0785491256"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://www.titito.pro/upload/photos/2019/04/02/15/20/ai4dg97v61.jpg",
+                  35.55597, 6.17414,"Batna","Bouaziz Amina","0625187559"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://prescriptor.info/images/167.jpg",
+                  36.2675, 2.7500,"Medea","Benyahia Wafa", "0689564123"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIDI3vcwWV6ZcrNUNE8fw9FrZqRiXHY50m2A&usqp=CAU",
+                  35.69906, -0.63588,"Oran","Zitouni Aya","0568565422" ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _boxes(String _image, double lat,double long,String storeName,nameReseller,Phonenumber) {
+    return  GestureDetector(
+      onTap: () {
+        _gotoLocation(lat,long);
+      },
+      child:Container(
+        child: new FittedBox(
+          child: Material(
+              color: Colors.white,
+              elevation: 8.0,
+              borderRadius: BorderRadius.circular(24.0),
+              shadowColor: Colors.green,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: new BorderRadius.circular(24.0),
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(_image),
+                      ),
+                    ),),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: myDetailsContainer1(storeName,nameReseller,Phonenumber),
+                    ),
+                  ),
+
+                ],)
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget myDetailsContainer1(String storeName,String nameReseller,String Phonenumber) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+              child: Text(storeName,
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold),
+              )),
+        ),
+        SizedBox(height:5.0),
+        Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                    child: Text(
+                      "4.1",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18.0,
+                      ),
+                    )),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+              ],
+            )),
+        SizedBox(height:5.0),
+        Container(
+            child: Text(
+              nameReseller,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18.0,
+                  fontWeight: FontWeight.bold
+              ),
+            )),
+        SizedBox(height:5.0),
+        Container(
+            child: Text(
+              Phonenumber,
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                  ),
+            )),
+      ],
+    );
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition:  CameraPosition(target: LatLng(36.73225, 3.08746), zoom: 4.5),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: {
+          BatnaMarker,MedeaMarker,OranMarker,AlgiersMarker,OuarglaMarker,AnnabaMarker
+        },
+      ),
+    );
+  }
+
+  Future<void> _gotoLocation(double lat,double long) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
+      bearing: 45.0,)));
+  }
+}
+
+Marker AlgiersMarker = Marker(
+  markerId: MarkerId('Algiers'),
+  position: LatLng(36.73225, 3.08746),
+  infoWindow: InfoWindow(title: 'Manel Bahi'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+
+Marker OuarglaMarker = Marker(
+  markerId: MarkerId('Ouargla'),
+  position: LatLng(31.9500, 5.3167),
+  infoWindow: InfoWindow(title: 'Benhamadi Yasmine'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+Marker AnnabaMarker = Marker(
+  markerId: MarkerId('Annaba'),
+  position: LatLng(36.9, 7.76667),
+  infoWindow: InfoWindow(title: 'Kermiche Asma'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+
+Marker BatnaMarker = Marker(
+  markerId: MarkerId('Batna'),
+  position: LatLng(35.55597, 6.17414),
+  infoWindow: InfoWindow(title: 'Bouaziz Amina'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+Marker MedeaMarker = Marker(
+  markerId: MarkerId('Medea'),
+  position: LatLng(36.2675, 2.7500),
+  infoWindow: InfoWindow(title: 'Benyahia Wafa'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+Marker OranMarker = Marker(
+  markerId: MarkerId('Oran'),
+  position: LatLng(35.69906, -0.63588),
+  infoWindow: InfoWindow(title: 'Zitouni Aya'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
